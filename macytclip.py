8d28ed9f173d0aed5eb67c5cea88e39e48d5d2ba
MasterMax13124 <mastermax13124@gmail.com>
2021-03-12 13:44:18 +0100

    Added notification for failed download

--- macytclip.py
+++ macytclip.py
@@ -7,20 +7,25 @@ import os
 # Author: MasterMax13124
 # Date: 12.Mar 2021
 # Notes: Proof of concept completed. Rumps notifications seem broken, so I am using klaxon
-# To-Do: Expand download options, add notification for failed ytdl, add better icon and config options
+# To-Do: Expand download options, add better icon and config options
 
 class macytclipapp():
     def __init__(self):
         self.app = rumps.App("MacYTclip", "👇")
         self.download_button = rumps.MenuItem(title="Download", callback=self.download_from_clipboard)
-        self.app.menu = [self.download_button]
+        self.title_entry = rumps.MenuItem(title="macytclip v1.0", callback=None)
+        self.app.menu = [self.title_entry, self.download_button]
+        
     
     def download_from_clipboard(self, sender):
-        klaxon("Download started", "macytclip", "Youtube-dl is initializing")
+        klaxon("Download started", "macytclip", "")
         ydl_opts = {}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([clipboard.paste()])
-        klaxon("Download complete!", "macytclip", "Check Finder")
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([clipboard.paste()])
+            klaxon("Download complete!", "macytclip", "Check your Downloads folder!")
+        except:
+            klaxon("The copied text was not a valid url!", "macytclip")
 
     def run(self):
         self.app.run()
