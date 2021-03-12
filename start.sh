90b9d4a179dcb77ad58fda056a179c8573c2f309
MasterMax13124 <mastermax13124@gmail.com>
2021-03-12 15:14:30 +0100

    Modified and renamed start.sh

--- start.sh
+++ start.sh
@@ -4,3 +4,7 @@ python3 -m venv macytclip
 source macytclip/bin/activate
 pip install py2app klaxon clipboard rumps youtube-dl
 python3 setup.py py2app
+deactivate
+mv dist/macytclip.app macytclip.app
+rm -rf dist
+rm -rf build
