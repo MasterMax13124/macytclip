4f77688b6e0c2aac7bc2ed6fdb1f99011b0ba01a
MasterMax13124 <mastermax13124@gmail.com>
2021-03-14 11:42:12 +0100

    Implemented H.264 option

--- macytclip.py
+++ macytclip.py
@@ -11,21 +11,31 @@ import os
 
 class macytclipapp():
     def __init__(self):
-        self.app = rumps.App("MacYTclip", "👇")
-        self.download_button = rumps.MenuItem(title="Download", callback=self.download_from_clipboard)
-        self.title_entry = rumps.MenuItem(title="macytclip v1.0", callback=None)
-        self.app.menu = [self.title_entry, self.download_button]
+        self.app = rumps.App("MacYTclip", "⤓")
+        # self.download_button = rumps.MenuItem(title="Download", callback=self.download_from_clipboard)
+        self.title_entry = rumps.MenuItem(title="macytclip v1.1", callback=None)
+        self.download_h264 = rumps.MenuItem(title="Download video (H.264)", callback=self.download_h264_function)
+        self.app.menu = [self.title_entry, self.download_h264]
         
-    
-    def download_from_clipboard(self, sender):
+    def download_h264_function(self, sender):
         klaxon("Download started", "macytclip", "")
-        ydl_opts = {}
+        ydl_opts = {'format' : 'bestvideo+bestaudio[ext=m4a]/best', 'merge_output_format': 'mp4', 'nocheckcertificate': 'true'}
         try:
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([clipboard.paste()])
             klaxon("Download complete!", "macytclip", "Check your Downloads folder!")
         except:
             klaxon("The copied text was not a valid url!", "macytclip")
+    
+    # def download_from_clipboard(self, sender):
+    #     klaxon("Download started", "macytclip", "")
+    #     ydl_opts = {}
+    #     try:
+    #         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    #             ydl.download([clipboard.paste()])
+    #         klaxon("Download complete!", "macytclip", "Check your Downloads folder!")
+    #     except:
+    #         klaxon("The copied text was not a valid url!", "macytclip")
 
     def run(self):
         self.app.run()
